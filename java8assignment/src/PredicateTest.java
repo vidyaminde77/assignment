f9d36ffadc1b04aa8d1db56a7f4984a59bb4bb84
vidyaminde77 <108066830+vidyaminde77@users.noreply.github.com>
2022-06-28 11:30:40 +0530

    Delete PredicateTest.java

--- java8assignment/src/PredicateTest.java
+++
@@ -1,9 +0,0 @@
-
-import java.util.function.Predicate; 
-public class PredicateTest { 
-  public static void main(String[] args)
-{ 
- Predicate < String > notNull = ((Predicate < String > )(arg -> arg == null)).negate(); 
- System.out.println(notNull.test(null)); 
- }
-}
